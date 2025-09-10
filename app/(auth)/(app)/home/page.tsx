3c504791e3d304ddc0ae119452362177b50270a1
Sayantan Chowdhury <shyuhaspivot@gmail.com>
2025-09-10 15:07:27 +0530

    completed

--- app/(auth)/(app)/home/page.tsx
+++ app/(auth)/(app)/home/page.tsx
@@ -1,13 +1,84 @@
 "use client"
-
-import React from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
+import axios from 'axios'
+import VideoCard from '@/components/VideoCard'
+import { Video } from '@/types'
 
 function Home() {
+  const [videos, setVideos] = useState<Video[]>([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+
+    const fetchVideos = useCallback(async () => {
+        try {  //get the videos
+            const response = await axios.get("/api/videos")
+            if(Array.isArray(response.data)) {
+                setVideos(response.data)
+            } else {
+                throw new Error(" Unexpected response format");
+
+            }
+        } catch (error) {
+            console.log(error);
+            setError("Failed to fetch videos")
+
+        } finally {
+            setLoading(false)
+        }
+    }, [])
+
+    useEffect(() => {
+        fetchVideos()
+    }, [fetchVideos])
+    //change in fetch videos then rerun the method
+
+     const handleDownload = useCallback((url: string, title: string) => {
+        () => {
+            const link = document.createElement("a");
+            link.href = url;
+            link.setAttribute("download", `${title}.mp4`);
+            link.setAttribute("target", "_blank");
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            //download the video
+
+        }
+
+    }, [])
+
+    if(loading) {
+      return <div>Loading...</div>
+    }
+
+
+
   return (
-    <div>
-      Home
-    </div>
-  )
+        <div className="container mx-auto p-4">
+          <h1 className="text-2xl font-bold mb-4">Videos</h1>
+          {videos.length === 0 ? (
+            <div className="text-center text-lg text-gray-500">
+              No videos available
+            </div>
+          ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              {
+                videos.map((video) => (
+                    <VideoCard
+                        key={video.id}
+                         video={{
+      ...video,
+      originalSize: String(video.originalSize),
+      compressedSize: String(video.compressedSize),
+    }}
+                        onDownload={handleDownload}
+                    />
+                ))
+              }
+            </div>
+          )}
+        </div>
+      );
 }
 
 export default Home
